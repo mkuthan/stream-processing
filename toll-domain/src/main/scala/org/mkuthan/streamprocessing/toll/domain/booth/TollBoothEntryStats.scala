081b59acb2ab33e4f13ee02253241fd61b9ad236
Marcin Kuthan <marcin.kuthan@gmail.com>
2022-11-06 21:44:27 +0100

    TollBoothEntryStats basic test

--- toll-domain/src/main/scala/org/mkuthan/streamprocessing/toll/domain/booth/TollBoothEntryStats.scala
+++ toll-domain/src/main/scala/org/mkuthan/streamprocessing/toll/domain/booth/TollBoothEntryStats.scala
@@ -4,6 +4,7 @@ import com.spotify.scio.bigquery.types.BigQueryType
 import com.spotify.scio.coders.Coder
 import com.spotify.scio.values.SCollection
 
+import com.twitter.algebird.Semigroup
 import org.joda.time.Duration
 import org.joda.time.Instant
 
@@ -29,9 +30,36 @@ object TollBoothEntryStats {
       total_toll: String
   )
 
+  object TollBoothEntrySemigroup extends Semigroup[TollBoothEntryStats] {
+    override def plus(x: TollBoothEntryStats, y: TollBoothEntryStats): TollBoothEntryStats = {
+      require(x.id == y.id)
+
+      TollBoothEntryStats(
+        id = x.id,
+        beginTime = if (x.beginTime.isBefore(y.beginTime)) x.beginTime else y.beginTime,
+        endTime = if (x.endTime.isAfter(y.endTime)) x.endTime else y.endTime,
+        count = x.count + y.count,
+        totalToll = x.totalToll + y.totalToll
+      )
+    }
+  }
+
   def calculateInFixedWindow(input: SCollection[TollBoothEntry], duration: Duration): SCollection[TollBoothEntryStats] =
-    input.context.empty[TollBoothEntryStats]()
+    input
+      .keyBy(_.id)
+      .mapValues(fromBoothEntry)
+      .withFixedWindows(duration)
+      .sumByKey(TollBoothEntrySemigroup)
+      .values
 
   def encode(input: SCollection[TollBoothEntryStats]): SCollection[Raw] =
     input.context.empty[Raw]()
+
+  def fromBoothEntry(boothEntry: TollBoothEntry): TollBoothEntryStats = TollBoothEntryStats(
+    id = boothEntry.id,
+    beginTime = boothEntry.entryTime,
+    endTime = boothEntry.entryTime,
+    count = 1,
+    totalToll = boothEntry.toll
+  )
 }
---
+++ toll-domain/src/test/scala/org/mkuthan/streamprocessing/toll/domain/booth/TollBoothEntryStatsTest.scala
@@ -0,0 +1,61 @@
+package org.mkuthan.streamprocessing.toll.domain.booth
+
+import com.spotify.scio.testing.testStreamOf
+import com.spotify.scio.testing.PipelineSpec
+import com.spotify.scio.testing.TestStreamScioContext
+
+import org.apache.beam.sdk.values.TimestampedValue
+import org.joda.time.Duration
+import org.joda.time.Instant
+
+import org.mkuthan.streamprocessing.shared.test.scio.TimestampedMatchers
+
+class TollBoothEntryStatsTest extends PipelineSpec
+    with TimestampedMatchers
+    with TollBoothEntryFixture {
+
+  import TollBoothEntryStats._
+
+  private val FiveMinutes = Duration.standardMinutes(5)
+
+  "TollBoothEntryStats" should "be calculated" in runWithContext { sc =>
+    val tollBoothId = TollBoothId("1")
+
+    val tollBoothEntry1 = anyTollBoothEntry.copy(
+      id = tollBoothId,
+      entryTime = Instant.parse("2014-09-10T12:01:00.000Z"),
+      toll = BigDecimal(2)
+    )
+    val tollBoothEntry2 = anyTollBoothEntry.copy(
+      id = tollBoothId,
+      entryTime = Instant.parse("2014-09-10T12:01:30.000Z"),
+      toll = BigDecimal(1)
+    )
+    val tollBoothEntry3 = anyTollBoothEntry.copy(
+      id = tollBoothId,
+      entryTime = Instant.parse("2014-09-10T12:04:00.000Z"),
+      toll = BigDecimal(3)
+    )
+
+    val inputs = testStreamOf[TollBoothEntry]
+      .addElements(TimestampedValue.of(tollBoothEntry1, tollBoothEntry1.entryTime))
+      .addElements(TimestampedValue.of(tollBoothEntry2, tollBoothEntry2.entryTime))
+      .addElements(TimestampedValue.of(tollBoothEntry3, tollBoothEntry3.entryTime))
+      .advanceWatermarkToInfinity()
+
+    val results = calculateInFixedWindow(sc.testStream(inputs), FiveMinutes)
+
+    results.withTimestamp should inOnTimePane("2014-09-10T12:00:00Z", "2014-09-10T12:05:00Z") {
+      containSingleValueAtTime(
+        "2014-09-10T12:04:59.999Z",
+        TollBoothEntryStats(
+          id = tollBoothId,
+          beginTime = Instant.parse("2014-09-10T12:01:00Z"),
+          endTime = Instant.parse("2014-09-10T12:04:00Z"),
+          count = 3,
+          totalToll = BigDecimal(2 + 1 + 3)
+        )
+      )
+    }
+  }
+}

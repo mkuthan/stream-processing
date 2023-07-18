b6c132b180bfc4f630489b2a4c34b8b0791a8c18
Marcin Kuthan <1560526+mkuthan@users.noreply.github.com>
2023-07-18 13:34:00 +0000

    Update scio-core to 0.14.0-dev1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scio = "com.spotify" %% "scio-core" % "0.13.0"
+  val scio = "com.spotify" %% "scio-core" % "0.14.0-dev1"
   val scioGcp = "com.spotify" %% "scio-google-cloud-platform" % "0.13.0"
   val scioTest = "com.spotify" %% "scio-test" % "0.13.0"
 

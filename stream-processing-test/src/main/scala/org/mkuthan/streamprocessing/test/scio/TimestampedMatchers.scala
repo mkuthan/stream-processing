dffeec1d51e69e5f7f49aa5745766e6ef644af9f
Marcin Kuthan <marcin.kuthan@allegro.pl>
2023-09-04 20:22:05 +0200

    Fixes after review

--- stream-processing-test/src/main/scala/org/mkuthan/streamprocessing/test/scio/TimestampedMatchers.scala
+++ stream-processing-test/src/main/scala/org/mkuthan/streamprocessing/test/scio/TimestampedMatchers.scala
@@ -14,6 +14,16 @@ import org.scalatest.matchers.Matcher
 trait TimestampedMatchers extends InstantSyntax {
   this: SCollectionMatchers =>
 
+  def inWindow[T: ClassTag](begin: String, end: String)(matcher: MatcherBuilder[T]): Matcher[T] = {
+    val window = new IntervalWindow(begin.toInstant, end.toInstant)
+    matcher match {
+      case value: SingleMatcher[_, _] =>
+        value.matcher(_.inWindow(window))
+      case value: IterableMatcher[_, _] =>
+        value.matcher(_.inWindow(window))
+    }
+  }
+
   def inOnTimePane[T: ClassTag](begin: String, end: String)(matcher: MatcherBuilder[T]): Matcher[T] =
     inOnTimePane(new IntervalWindow(begin.toInstant, end.toInstant))(matcher)
 
@@ -26,8 +36,15 @@ trait TimestampedMatchers extends InstantSyntax {
   def inFinalPane[T: ClassTag](begin: String, end: String)(matcher: MatcherBuilder[T]): Matcher[T] =
     inFinalPane(new IntervalWindow(begin.toInstant, end.toInstant))(matcher)
 
-  def inWindow[T: ClassTag, B: ClassTag](begin: String, end: String)(matcher: IterableMatcher[T, B]): Matcher[T] =
-    inWindow(new IntervalWindow(begin.toInstant, end.toInstant))(matcher)
+  def inOnlyPane[T: ClassTag](begin: String, end: String)(matcher: MatcherBuilder[T]): Matcher[T] = {
+    val window = new IntervalWindow(begin.toInstant, end.toInstant)
+    matcher match {
+      case value: SingleMatcher[_, _] =>
+        value.matcher(_.inOnlyPane(window))
+      case value: IterableMatcher[_, _] =>
+        value.matcher(_.inOnlyPane(window))
+    }
+  }
 
   /**
    * Assert that the SCollection contains the provided element at given time without making assumptions about other

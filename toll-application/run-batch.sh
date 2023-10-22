1a058a9bed645e3c199612e10dc5931e530ae1aa
Marcin Kuthan <marcin.kuthan@gmail.com>
2023-10-22 20:36:29 +0200

    Fix deployment

---
+++ toll-application/run-batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJECT=playground-272019
+REGION=europe-west1
+DATE=`date +%Y-%m-%d`
+
+gcloud dataflow flex-template run "toll-application-`date +%Y%m%d-%H%M%S`" \
+    --template-file-gcs-location "gs://$PROJECT-toll-application/templates/toll-application-batch.json" \
+    --region "$REGION" \
+    --staging-location="gs://$PROJECT-toll-application/staging/" \
+    --parameters effectiveDate="$DATE" \
+    --parameters entryTable="$PROJECT.toll_application.toll-booth-entry" \
+    --parameters exitTable="$PROJECT.toll_application.toll-booth-exit" \
+    --parameters vehicleRegistrationTable="$PROJECT.toll_application.vehicle-registration" \
+    --parameters entryStatsHourlyTable="$PROJECT.toll_application.toll-booth-entry-stats-hourly" \
+    --parameters entryStatsDailyTable="$PROJECT.toll_application.toll-booth-entry-stats-daily" \
+    --parameters totalVehicleTimesOneHourGapTable="$PROJECT.toll_application.total-vehicle-times-one-hour-gap" \
+    --parameters totalVehicleTimesOneHourGapDiagnosticTable="$PROJECT.toll_application.total-vehicle-times-one-hour-gap-diagnostic" \
+    --parameters vehiclesWithExpiredRegistrationDailyTable="$PROJECT.toll_application.vehicles-with-expired-registration-diagnostic-daily" \
+    --parameters vehiclesWithExpiredRegistrationDailyDiagnosticTable="$PROJECT.toll_application.vehicles-with-expired-registration-daily-diagnostic" \
+

cfc1118a13e5426d9fc3bb90b1a22b9f3882216c
Marcin Kuthan <marcin.kuthan@gmail.com>
2023-10-23 14:55:28 +0200

    flex templates - 1

--- toll-application/run-batch.sh
+++ toll-application/run-batch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+DATE=`date +%Y-%m-%d`
+
 PROJECT=playground-272019
 REGION=europe-west1
-DATE=`date +%Y-%m-%d`
 
 gcloud dataflow flex-template run "toll-application-`date +%Y%m%d-%H%M%S`" \
     --template-file-gcs-location "gs://$PROJECT-toll-application/templates/toll-application-batch.json" \
     --region "$REGION" \
-    --staging-location="gs://$PROJECT-toll-application/staging/" \
-    --additional-experiments="use_runner_v2" \
-    --max-workers=1 \
+    --staging-location "gs://$PROJECT-toll-application/staging/" \
+    --additional-experiments "use_runner_v2" \
+    --max-workers 1 \
+    --worker-machine-type "t2d-standard-1" \
     --parameters effectiveDate="$DATE" \
     --parameters entryTable="$PROJECT.toll_application.toll-booth-entry" \
     --parameters exitTable="$PROJECT.toll_application.toll-booth-exit" \
@@ -18,5 +20,5 @@ gcloud dataflow flex-template run "toll-application-`date +%Y%m%d-%H%M%S`" \
     --parameters entryStatsDailyTable="$PROJECT.toll_application.toll-booth-entry-stats-daily" \
     --parameters totalVehicleTimesOneHourGapTable="$PROJECT.toll_application.total-vehicle-times-one-hour-gap" \
     --parameters totalVehicleTimesOneHourGapDiagnosticTable="$PROJECT.toll_application.total-vehicle-times-one-hour-gap-diagnostic" \
-    --parameters vehiclesWithExpiredRegistrationDailyTable="$PROJECT.toll_application.vehicles-with-expired-registration-diagnostic-daily" \
+    --parameters vehiclesWithExpiredRegistrationDailyTable="$PROJECT.toll_application.vehicles-with-expired-registration-daily" \
     --parameters vehiclesWithExpiredRegistrationDailyDiagnosticTable="$PROJECT.toll_application.vehicles-with-expired-registration-daily-diagnostic" \

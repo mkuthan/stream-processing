b3767065749c8627291779820819facc411b9da9
Marcin Kuthan <marcin.kuthan@allegro.pl>
2024-11-25 10:27:59 +0100

    Update dependencies and plugins to latest versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,18 @@
 import sbt.*
 
 object Dependencies {
-  val scio = "com.spotify" %% "scio-core" % "0.14.7"
+  val scio = "com.spotify" %% "scio-core" % "0.14.9"
 
-  val scioGcp = "com.spotify" %% "scio-google-cloud-platform" % "0.14.7" excludeAll (
+  val scioGcp = "com.spotify" %% "scio-google-cloud-platform" % "0.14.9" excludeAll (
     ExclusionRule(organization = "org.apache.beam", name = "beam-runners-direct-java")
   )
 
-  val scioTest = "com.spotify" %% "scio-test" % "0.14.7" excludeAll (
+  val scioTest = "com.spotify" %% "scio-test" % "0.14.9" excludeAll (
     ExclusionRule(organization = "org.apache.beam", name = "beam-runners-direct-java")
   )
 
-  val beamDirectRunner = "org.apache.beam" % "beam-runners-direct-java" % "2.58.1"
-  val beamDataflowRunner = "org.apache.beam" % "beam-runners-google-cloud-dataflow-java" % "2.58.1"
+  val beamDirectRunner = "org.apache.beam" % "beam-runners-direct-java" % "2.60.0"
+  val beamDataflowRunner = "org.apache.beam" % "beam-runners-google-cloud-dataflow-java" % "2.60.0"
 
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
 
@@ -22,7 +22,7 @@ object Dependencies {
   val logback = "ch.qos.logback" % "logback-classic" % "1.5.7"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
-  val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.18.0"
+  val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-18" % "3.2.19.0"
 
   val magnolifyScalaCheck = "com.spotify" %% "magnolify-scalacheck" % "0.7.4"
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,7 +3,7 @@ addSbtPlugin("com.github.sbt" % "sbt-jacoco" % "3.5.0")
 // code formatting
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
 // code analysis
-addSbtPlugin("com.sksamuel.scapegoat" %% "sbt-scapegoat" % "1.2.4")
+addSbtPlugin("com.sksamuel.scapegoat" %% "sbt-scapegoat" % "1.2.7")
 // automated code changes
 addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.12.1")
 // fat-jar for flex templates
